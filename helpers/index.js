44a10b58de2b15c2db4a168896eaac7c19b641df
JuliaOraz <uliaoraz@gmail.com>
2023-01-06 18:20:40 +0200

    clear files

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,9 @@
 const HttpError = require("./HttpError");
 const controllerWrapp = require("./controllerWrapp");
 const handleMongooseError = require("./handleMongooseError");
-const resizeAva = require("./resizeAva");
 
 module.exports = {
   HttpError,
   controllerWrapp,
   handleMongooseError,
-  resizeAva,
 };

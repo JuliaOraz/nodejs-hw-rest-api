e99acb6c0314fceb36fda8742735eb580167c0f2
JuliaOraz <uliaoraz@gmail.com>
2023-01-06 17:06:53 +0200

    add homework

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const controllerWrapp = require("./controllerWrapp");
 const handleMongooseError = require("./handleMongooseError");
+const resizeAva = require("./resizeAva");
 
 module.exports = {
   HttpError,
   controllerWrapp,
   handleMongooseError,
+  resizeAva,
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
 const { validateBody, validateBodyFavorite } = require("./validateBody");
 const validateParams = require("./validateParams");
-const authenticate = require("../middlewares/authenticate");
+const authenticate = require("./authenticate");
+const uploadAvatar = require("./uploadAvatar");
 
 module.exports = {
   validateBody,
   validateParams,
   validateBodyFavorite,
   authenticate,
+  uploadAvatar,
 };
